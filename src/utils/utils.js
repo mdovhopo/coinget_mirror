0474447f05610db8d011255296a54d4cbddc1f42
mdovhopo <mdovhopo@student.unit.ua>
2019-08-05 01:57:08 +0300

    Add animation for Side sections in about section

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,8 +1,14 @@
 export const isEmpty = (obj) => {
-  for(const key in obj) {
-    if(obj.hasOwnProperty(key))
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key))
       return false;
   }
   return true;
 };
 
+export const isElementVisible = (el) => {
+  const {offsetTop, offsetHeight} = el;
+  const {pageYOffset} = window;
+  return offsetTop < innerHeight + pageYOffset &&
+    offsetTop + offsetHeight > pageYOffset
+};
