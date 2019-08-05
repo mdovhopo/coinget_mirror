6403cb5ea0cf9eec876f4ab0d86bea97dfab0545
mdovhopo <mdovhopo@student.unit.ua>
2019-08-05 11:59:23 +0300

    update isElementVisible util function.

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -7,8 +7,18 @@ export const isEmpty = (obj) => {
 };
 
 export const isElementVisible = (el) => {
-  const {offsetTop, offsetHeight} = el;
-  const {pageYOffset} = window;
-  return offsetTop < innerHeight + pageYOffset &&
-    offsetTop + offsetHeight > pageYOffset
+  if (!el || 1 !== el.nodeType) {
+    return false;
+  }
+  const html = document.documentElement;
+  const r = el.getBoundingClientRect();
+
+  const clHeight = html.clientHeight;
+
+  return (!!r
+    && r.bottom >= 0
+    && r.right >= 0
+    && r.top <= clHeight
+    && r.left <= html.clientWidth
+  );
 };
